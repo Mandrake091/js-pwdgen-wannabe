2c7794b4b45bb5c6acd852532b18ea66f359e5a1
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-23 15:55:54 +0100

    Added flowchart

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+/*
+-(Password Generator)
+A[Chiedo all'utente il nome]-->
+B[Chiedo all'utente il cognome]-->
+D[Chiedo all'utente il colore preferito]-->
+E[Chiedo all'utente la data di nascita]-->
+C[Lo saluto]-->
+F[Ottengo la sua password dalla somma dei dati che inserisce]
+*/
+
 const userName = prompt('Nome?');
 console.log(userName);
 
@@ -10,7 +20,7 @@ console.log(favoriteColor);
 const dateOfBirth = prompt('Data di nascita?');
 console.log(dateOfBirth);
 
+document.getElementById('userName').innerHTML += ' ' + userName + ' ' + userSurname + '!';
 
 document.getElementById('password').innerHTML += ' ' + userName + userSurname + favoriteColor + dateOfBirth;
 
-document.getElementById('userName').innerHTML += ' ' + userName + ' ' + userSurname + '!';
